2ead33195a5579fa842b38d0351686505349dc82
Anand Biswas <anandmaya48@gmail.com>
2020-05-31 09:43:07 +0530

    security

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/', include('users.urls')),
